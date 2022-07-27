ed2c3aba7edd4032e19b3d3617ce7e879d852fb8
dorian <dorianlegan@protonmail.com>
2022-07-27 07:26:53 +0100

    form state setup

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,6 +3,9 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../sidebar/Sidebar";
 import "./home.css";
 const Home = () => {
+  /*Outlet is new to me, I needed to find a way to allow 
+    the sidebar to be present in all routes but still hidden if not authenticated
+     */
   return (
     <div className="home">
       <Sidebar />
--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,12 +1,16 @@
-import { useState } from "react";
-const useInput = () => {
+import { useEffect, useState } from "react";
+const useInput = (type) => {
   const [value, setValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  useEffect(() => {
+    setIsValid(value.length > 0);
+  }, [value]);
   const reset = () => {
     setValue("");
   };
   const handleChange = (e) => {
     setValue(e.target.value);
   };
-  return [value, handleChange, reset];
+  return [value, handleChange, reset, isValid];
 };
 export default useInput;
