29a0ff017b2f684e47643578ea90249a59049ce7
dorian <dorianlegan@protonmail.com>
2022-07-31 10:13:33 +0100

    error pages bugfix

--- src/hooks/useDataLoad.js
+++ src/hooks/useDataLoad.js
@@ -30,6 +30,7 @@ const useDataLoad = (url, options) => {
   useEffect(() => {
     const startLoad = async () => {
       const jsonData = await fetchData(url, options, null);
+
       setData(jsonData);
       setLoading(false);
     };
