1705fb6350fab22e756f37edfcc18f2b1184a218
dorian <dorianlegan@protonmail.com>
2022-07-31 23:44:20 +0100

    tweaks to profile page

--- src/components/filepicker/FilePicker.js
+++ src/components/filepicker/FilePicker.js
@@ -1,56 +1,43 @@
 import { useContext, useRef, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
-const FilePicker = () => {
+const FilePicker = (props) => {
   const [icon, setIcon] = useState("ri-add-box-line");
   const hiddenFileSelect = useRef(null);
   const { user, token } = useContext(AuthContext);
 
-  const handleFile = (e) => {
+  const handleFile = async (e, cb) => {
     const input = e.target.files[0];
-
-    // This will upload the file after having read it
-
-    fetch(
+    const formData = new FormData();
+    formData.append("file", input);
+    const options = {
+      method: "POST",
+      body: formData,
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "*/*",
+      },
+    };
+    await fetch(
       `https://interview.intrinsiccloud.net/profile/profileImage?name=${user}`,
-      {
-        // Your POST endpoint
-        method: "POST",
-        headers: {
-          // Content-Type may need to be completely **omitted**
-          // or you may need something
-          "Content-Type": "multipart/form-data",
-          Authorization: `Bearer ${token}`,
-        },
-        body: input, // This is your file object
-      }
-    )
-      .then(
-        (response) => response.json() // if the response is a JSON object
-      )
-      .then(
-        (success) => console.log(success) // Handle the success response object
-      )
-      .catch(
-        (error) => console.log(error) // Handle the error response object
-      );
+      options
+    );
+    props.refresh();
   };
 
   return (
-    <i
-      className={icon}
-      onClick={() => hiddenFileSelect.current.click()}
-      onMouseOver={() => setIcon("ri-add-box-fill")}
-      onMouseLeave={() => setIcon("ri-add-box-line")}
-    >
-      <input
-        ref={hiddenFileSelect}
-        type="file"
-        accept=".png, .jpg, .jpeg"
-        onChange={handleFile}
-        style={{ display: "none" }}
-      />
-    </i>
+    <div className="filepicker-wrap">
+      <i className={icon} onClick={() => hiddenFileSelect.current.click()}>
+        <input
+          ref={hiddenFileSelect}
+          type="file"
+          accept=".png, .jpg, .jpeg"
+          onChange={handleFile}
+          style={{ display: "none" }}
+        />
+      </i>
+    </div>
   );
 };
 
